3ac7f06d88965a5ca5d00cd341d2a03664603064
Disha79009 <disha.reshu13@gmail.com>
2025-04-03 23:41:05 +0530

    Update inquiry.js

--- routes/inquiry.js
+++ routes/inquiry.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Contact = require('../models/Inquiry');
 const router = express.Router();
+const Inquiry = require("../models/Inquiry");  // Make sure this is correct
 
 router.post('/', async (req, res) => {
     try {
@@ -16,7 +17,7 @@ router.post('/', async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     const inquiries = await Inquiry.find(); // Fetch all inquiries from MongoDB
-    res.status(200).json(inquiries);
+    res.status(200).json(inquiries); // Return inquiries as JSON
   } catch (error) {
     res.status(500).json({ error: "Server error" });
   }
