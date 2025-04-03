195541979f4c45f43bb6e3d97490e083d2e462d8
Disha79009 <disha.reshu13@gmail.com>
2025-04-03 21:24:54 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/ORCAS_DB", {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
+
         console.log("✅ MongoDB Connected Successfully!");
     } catch (error) {
-        console.error("❌ MongoDB Connection Failed:", error);
+        console.error(`❌ MongoDB Connection Failed: ${error.message}`);
         process.exit(1);
     }
 };
